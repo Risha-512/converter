1910943fb54fa1d9febbfa9ee947631535e1f340
Rishana125 <marinka.lagunova@gmail.com>
2020-02-19 17:20:00 +0700

    Add converter class template

---
+++ ClassLibrary/Converter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Converter
+    {
+        public string Convert(string data, int origNumSyst, int resNumSyst)
+        {
+            return "";
+        }
+
+        private int ConvertNumberToDec(string num)
+        {
+            return 0;
+        }
+
+        private string ConvertDecimalNumber(int num, int numberSystem)
+        {
+            return "";
+        }
+    }
+}
